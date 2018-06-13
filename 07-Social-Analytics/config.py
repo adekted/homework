220d7cec21c16d9756b2ce1c5f7df6999843dfe8
adekted <aymercad@uci.edu>
2018-06-12 21:21:23 -0700

    halfway through social analytics

---
+++ 07-Social-Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "axQHGowdCKDuOLkkzbFgiVZxJ"
+consumer_secret = "7GVFiuCElkUSTOX47kxoElJtil0Ck1BQ6AgTMs0Atod4pNARRC"
+access_token = "98013922-LAzyeGANefCxuwDcxO1wnGa2isN47GxbZEjvSOsfe"
+access_token_secret = "D5QtF0QvpgjEUxtXlyi0dh4H6WmLTomFrEkgGSPCoskWL"
