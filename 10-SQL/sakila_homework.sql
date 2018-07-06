cd266001d9154426a332cd0617193a954f27784e
adekted <aymercad@uci.edu>
2018-07-05 23:47:30 -0700

    3/4 sql

--- 10-SQL/sakila_homework.sql
+++ 10-SQL/sakila_homework.sql
@@ -50,25 +50,59 @@ GROUP BY last_name;
 SELECT last_name, COUNT(last_name)
 FROM actor
 GROUP BY last_name
-HAVING COUNT(last_name) > 1;
+HAVING COUNT(last_name) >= 1;
 
 # Q4C - Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE first_name = 'GROUCHO' AND last_name = 'WILLIAMS';
 
 # Q4D - Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO GROUCHO, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! (Hint: update the record using a unique identifier.)
+UPDATE actor
+SET first_name = 'GROUCHO'
+WHERE first_name = 'HARPO' AND last_name = 'WILLIAMS';
 
 # Q5A - You cannot locate the schema of the address table. Which query would you use to re-create it?
+SHOW CREATE TABLE address;
 
 # Q6A - Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+SELECT s.first_name, s.last_name, a.address 
+FROM staff s INNER JOIN address a
+ON s.address_id = a.address_id;
 
 # Q6B - Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+SELECT s.first_name, s.last_name, SUM(p.amount) AS 'Sum Amount'
+FROM staff s INNER JOIN payment p 
+ON s.staff_id = p.staff_id
+GROUP BY s.staff_id;
 
 # Q6C - List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+SELECT f.title, f.film_id, COUNT(fa.actor_id) 
+FROM film f INNER JOIN film_actor fa
+ON f.film_id = fa.film_id
+GROUP BY f.film_id;
 
 # Q6D - How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT f.title, i.film_id, COUNT(i.store_id) AS 'Copies'
+FROM film f INNER JOIN inventory i
+ON f.film_id = i.film_id
+WHERE f.title = 'Hunchback Impossible';
 
 # Q6E - Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+SELECT c.customer_id, c.first_name, c.last_name, SUM(p.amount) AS 'Total Amount Paid'
+FROM customer c INNER JOIN payment p 
+ON c.customer_id = p.customer_id
+GROUP BY c.customer_id;
 
 # Q7A - The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+SELECT * 
+FROM film
+WHERE title LIKE 'K%' OR 'Q%' 
+AND language_id IN (
+	SELECT language_id
+    FROM language l
+    WHERE l.name = 'English'
+);
 
 # Q7B - Use subqueries to display all actors who appear in the film Alone Trip.
 
