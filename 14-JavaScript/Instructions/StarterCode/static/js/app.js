606b9a900c9f0fdc3d222715f8d8b668d375e532
adekted <aymercad@uci.edu>
2018-09-13 16:25:38 -0700

    finished js

--- 14-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var filterButton = d3.select('#filter-btn')
+
+filterButton.on("click", function(){
+    let filterDate = d3.select('#datetime').property('value');
+    let filteredData = tableData.filter(dates => dates.datetime === filterDate);
+    var tbody = tbody.append("tr");
+    filteredData.forEach(function(info){
+        var row = tbody.append("tr");
+        Object.entries(info).forEach(function ([key, value]) {
+            //     // Append a cell to the row for each value
+            var cell = tbody.append("td");
+            cell.text(value);
+          });
+    })
+})
