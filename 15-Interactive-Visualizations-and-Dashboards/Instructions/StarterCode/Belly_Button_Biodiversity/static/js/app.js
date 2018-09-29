99b4bea5ea6dc5e4ea3d112ca375f199f4d8d82c
adekted <aymercad@uci.edu>
2018-09-29 10:58:28 -0700

    finished belly button

--- 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,29 +1,154 @@
 function buildMetadata(sample) {
-
+  
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(`/metadata/${sample}`).then((metaData) =>{
+    const meta_data = metaData;
 
+    // Use d3 to select the panel with id of `#sample-metadata`
     // Use `.html("") to clear any existing metadata
+    var metaPanel = d3.select("#sample-metadata").html("")
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(meta_data).forEach((sample) => {
+      metaPanel.append('h6').text(`${sample[0]}: ${sample[1]}`)
+    })
+  })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    var level = parseFloat(wfreq) * 20;
+
+  // Trig to calc meter point
+  var degrees = 180 - level;
+  var radius = 0.5;
+  var radians = (degrees * Math.PI) / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+  var pathX = String(x);
+  var space = " ";
+  var pathY = String(y);
+  var pathEnd = " Z";
+  var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+  var data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      textinfo: "text",
+      textposition: "inside",
+      marker: {
+        colors: [
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
+
+  var layout = {
+    shapes: [
+      {
+        type: "path",
+        path: path,
+        fillcolor: "850000",
+        line: {
+          color: "850000"
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    },
+    yaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    }
+  };
+
+  var GAUGE = document.getElementById("gauge");
+  Plotly.newPlot(GAUGE, data, layout);
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  d3.json(`/samples/${sample}`).then((sampleData) => {
+    const sample_data = sampleData;
     // @TODO: Build a Bubble Chart using the sample data
+    var chartBubble = [{
+      x: sample_data.otu_ids,
+      y: sample_data.sample_values,
+      text: sample_data.otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_data.sample_values,
+        color: sample_data.otu_ids,
+        colorscale: "YlOrRd"
+      }
+    }];
 
+    var bubbleLayout = {
+      margin: {t: 5},
+      hovermode: 'closest',
+      xaxis: {title: 'OTU IDs'}
+    };
+
+    Plotly.newPlot("bubble", chartBubble, bubbleLayout)
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var pieData = [{
+      values: sample_data.sample_values.slice(0,15),
+      labels: sample_data.otu_ids.slice(0,15),
+      hovertext: sample_data.otu_labels.slice(0,15),
+      hoverinfo: "hovertext",
+      type: "pie"
+    }];
+
+    var pieLayout = {
+      margin: {t: 5, l: 5}
+    }
+    Plotly.newPlot("pie", pieData, pieLayout)
+  })
 }
 
 function init() {
